dec7ac2d17946c20e36b4bf73bd6145e958cdf26
Melanie Le <77984641+melaniele@users.noreply.github.com>
2024-07-22 21:15:04 -0400

    login page created for both Owner and Renter apps

--- RenterApp/App.js
+++ RenterApp/App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Login from "./screens/Login";
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const TabContainerComponent = () => (
+  <Tab.Navigator>
+    <Tab.Screen name="Create Rental" component={CreateRental} />
+  </Tab.Navigator>
+);
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Login"
+        screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen component={Login} name="Login" />
+        <Stack.Screen component={TabContainerComponent} name="Home" />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
