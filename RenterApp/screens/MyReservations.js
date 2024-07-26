3f3c09a00a15cd53c8206ffaea27ce181a57cb1f
DominikDolphin <dominik.thibaudeau@gmail.com>
2024-07-26 19:23:41 -0400

    Created auth component so that we can track logged in user on each screen

--- RenterApp/screens/MyReservations.js
+++ RenterApp/screens/MyReservations.js
@@ -1,8 +1,11 @@
 import { Text, View } from "react-native";
+import { useAuth } from "../components/AuthContext";
 
 export default function MyReservations() {
+  const { loggedInUserEmail } = useAuth();
     return (
       <View>
+        <Text>{loggedInUserEmail}</Text>
         <Text>My Reservations</Text>
       </View>
     );
