9adc14ecd5662853b82cb53023729d2bf7d0875b
DominikDolphin <dominik.thibaudeau@gmail.com>
2024-07-31 21:42:08 -0400

    added const to fetching function

--- RenterApp/screens/ConfirmReservation.js
+++ RenterApp/screens/ConfirmReservation.js
@@ -218,7 +218,7 @@ export default function ConfirmReservation({ route }) {
     }
   };
 
-  fetchAmountOfReservationsForMinimumDate = async () => {
+  const fetchAmountOfReservationsForMinimumDate = async () => {
     const reservationQuery = query(
       collection(db, 'reservationRequests'),
       where('listingID', '==', route.params.listingID)
