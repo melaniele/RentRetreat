c8087dc60701c8340f3b588dd30c31af96f8b714
Ankit <Ankiterram@gmail.com>
2024-08-01 22:03:03 -0400

    added signout button to MyReservation screen & fixed specific reservations for the city the user is in

--- RenterApp/screens/MyReservations.js
+++ RenterApp/screens/MyReservations.js
@@ -1,23 +1,72 @@
-import { Text, View, FlatList } from "react-native";
+import { Text, View, FlatList, TouchableOpacity, Alert } from "react-native";
 import { useAuth } from "../components/AuthContext";
 import { useEffect, useState } from "react"
-import { useIsFocused } from "@react-navigation/native"
-import { db } from '../firebaseConfig';
+import { useIsFocused, StackActions } from "@react-navigation/native"
+import { db, auth } from '../firebaseConfig';
+
 import { collection, getDocs, where, query } from "firebase/firestore"
 import { myReservationsStyles } from '../css/myReservationsStyles';
 import HouseInfo from "../components/HouseInfo";
+import { signOut } from 'firebase/auth';
 import UserInfo from "../components/UserInfo";
 
-export default function MyReservations() {
+export default function MyReservations({ navigation }) {
   const { loggedInUserEmail, userCityLocation } = useAuth();
-  
+
   const [reservationList, setReservationList] = useState([])
   const [isRefreshing, setIsRefreshing] = useState(false)
   const isUserOnThisScreen = useIsFocused()
 
+  const logoutPressed = async () => {
+    try {
+      await signOut(auth);
+      navigation.dispatch(StackActions.popToTop());
+    } catch (error) {
+      console.log(`error logging out: ${error}`);
+      Alert.alert(
+        'Error logging out',
+        `There was an error logging out: ${error}`
+      );
+    }
+  };
+
+  const setHeader = () => {
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity
+          onPress={() => {
+            logoutPressed();
+          }}
+          style={{
+            backgroundColor: '#2C3A47',
+            borderRadius: 5,
+            marginRight: 15,
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+            gap: 0,
+            padding: 5,
+            marginLeft: 15,
+            backgroundColor: '#fa8231',
+            padding: 7
+          }}
+        >
+          <Text
+            style={{
+              fontSize: 18,
+              color: 'black'
+            }}
+          >
+            Sign Out
+          </Text>
+        </TouchableOpacity>
+      )
+    });
+  };
+
   useEffect(() => {
     if (isUserOnThisScreen) {
-      console.log(userCityLocation)
+      setHeader();
       getDataFromDB()
     }
   }, [isUserOnThisScreen])
@@ -48,12 +97,11 @@ export default function MyReservations() {
     }
 
     try {
-      const updatedReservedListings = await Promise.all(
+      let updatedReservedListings = await Promise.all(
         listings.map(async (reservedListing) => {
           const listingsRef = query(collection(db, 'listings'), where("city", "==", userCityLocation));
           const querySnapshot = await getDocs(listingsRef);
           querySnapshot.forEach((doc) => {
-            console.log(doc.data().ownerEmail, reservedListing.ownerEmail, doc.id, reservedListing.listingID)
             if (
               doc.data().ownerEmail === reservedListing.ownerEmail &&
               doc.id === reservedListing.listingID
@@ -91,6 +139,10 @@ export default function MyReservations() {
           return reservedListing;
         })
       );
+
+      updatedReservedListings = updatedReservedListings.filter((listing) => {
+        return listing.city === undefined ? false : true;
+      });
 
       setReservationList(updatedReservedListings);
     } catch (error) {
